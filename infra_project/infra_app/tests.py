ba251da8d394ed43957bf4c1ac46cc47893b5718
Alex Gunyakov <regmeonly2020@gmail.com>
2022-06-27 09:40:19 +0800

    Running tests designed by the developer

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -8,7 +8,7 @@ class StaticPagesURLTests(TestCase):
         self.guest_client = Client()
 
     def test_about_url_exists_at_desired_location(self):
-        """Проверка доступности страниц."""
+        """Pages availability check."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
@@ -16,7 +16,7 @@ class StaticPagesURLTests(TestCase):
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_page_shows_correct_content(self):
-        """Проверка контента страниц."""
+        """Pages content check."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
 
